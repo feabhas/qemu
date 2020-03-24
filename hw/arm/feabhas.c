e2e3b1c57581624e576e558b4dc30a096b4a76f4
Niall Cooling <niall.cooling@gmail.com>
2020-03-24 11:43:18 +0000

    inital stm32f407 and feabhas config - clone of stm32f405

--- hw/arm/feabhas.c
+++ hw/arm/feabhas.c
@@ -27,14 +27,48 @@
 #include "hw/boards.h"
 #include "hw/qdev-properties.h"
 #include "qemu/error-report.h"
-#include "hw/arm/stm32f405_soc.h"
+#include "hw/arm/stm32f407_soc.h"
 #include "hw/arm/boot.h"
+
+#define NUM_IRQ_LINES 64
+#define NUM_OF_GPIOS 4
+
+static const uint32_t gpio_addr[NUM_OF_GPIOS] =
+    {0x40020000, 0x40020400, 0x40020800, 0x40020c00};
 
 static void feabhas_init(MachineState *machine)
 {
-    DeviceState *dev = qdev_create(NULL, TYPE_STM32F405_SOC);
+    DeviceState *dev = qdev_create(NULL, TYPE_STM32F407_SOC);
     qdev_prop_set_string(dev, "cpu-type", ARM_CPU_TYPE_NAME("cortex-m4"));
     object_property_set_bool(OBJECT(dev), true, "realized", &error_fatal);
+
+    // DeviceState *nvic;
+
+    // nvic = qdev_create(NULL, TYPE_ARMV7M);
+    // qdev_prop_set_uint32(nvic, "num-irq", NUM_IRQ_LINES);
+    // qdev_prop_set_string(nvic, "cpu-type", machine->cpu_type);
+    // qdev_prop_set_bit(nvic, "enable-bitband", true);
+    // object_property_set_link(OBJECT(nvic), OBJECT(get_system_memory()),
+    //                          "memory", &error_abort);
+    // /* This will exit with an error if the user passed us a bad cpu_type */
+    // qdev_init_nofail(nvic);
+
+    // qdev_connect_gpio_out_named(nvic, "SYSRESETREQ", 0,
+    //                             qemu_allocate_irq(&do_sys_reset, NULL, 0));
+
+    // DeviceState *gpio_dev[NUM_OF_GPIOS];
+    // for (unsigned i = 0; i < NUM_OF_GPIOS; ++i)
+    // {
+
+    //     gpio_dev[i] = sysbus_create_simple("feabhas", gpio_addr[i],
+    //                                        qdev_get_gpio_in(nvic,
+    //                                                         gpio_irq[i]));
+    //     // for (j = 0; j < 8; j++)
+    //     // {
+    //     //     gpio_in[i][j] = qdev_get_gpio_in(gpio_dev[i], j);
+    //     //     gpio_out[i][j] = NULL;
+    //     // }
+    // }
 
     armv7m_load_kernel(ARM_CPU(first_cpu),
                        machine->kernel_filename,
@@ -64,78 +98,78 @@ type_init(feabhas_machine_init);
 
 /////////////////////////////////////////////////////////////////////
 // DEVICE
-#define TYPE_FEABHAS_GPIO "feabhas-gpio"
-#define FEABHAS_GPIO(obj) OBJECT_CHECK(FeabhasGPIOState, (obj), TYPE_GPIOKEY)
+// #define TYPE_FEABHAS_GPIO "feabhas-gpio"
+// #define FEABHAS_GPIO(obj) OBJECT_CHECK(FeabhasGPIOState, (obj), TYPE_GPIOKEY)
 
-#define STM32_GPIO_PIN_COUNT 16
-typedef struct FeabhasGPIOState
-{
-    SysBusDevice parent_obj;
-    MemoryRegion iomem;
-    qemu_irq irq[STM32_GPIO_PIN_COUNT];
-} feabhas_gpio_state_t;
+// #define STM32_GPIO_PIN_COUNT 16
+// typedef struct FeabhasGPIOState
+// {
+//     SysBusDevice parent_obj;
+//     MemoryRegion iomem;
+//     qemu_irq irq[STM32_GPIO_PIN_COUNT];
+// } feabhas_gpio_state_t;
 
-#include "migration/vmstate.h"
-static const VMStateDescription vmstate_feabhas_gpio = {
-    .name = "feabhas_gpio",
-    .version_id = 1,
-    .minimum_version_id = 1,
-    // .fields = (VMStateField[]){
-    //     VMSTATE_END_OF_LIST()}
-};
+// #include "migration/vmstate.h"
+// static const VMStateDescription vmstate_feabhas_gpio = {
+//     .name = "feabhas_gpio",
+//     .version_id = 1,
+//     .minimum_version_id = 1,
+//     // .fields = (VMStateField[]){
+//     //     VMSTATE_END_OF_LIST()}
+// };
 
-static uint64_t stm32_gpio_read(void *opaque, uint64_t offset,
-                                unsigned size)
-{
-    return 0;
-}
-static void stm32_gpio_write(void *opaque, uint64_t offset,
-                             uint64_t value, unsigned size)
-{
-}
+// static uint64_t stm32_gpio_read(void *opaque, uint64_t offset,
+//                                 unsigned size)
+// {
+//     return 0;
+// }
+// static void stm32_gpio_write(void *opaque, uint64_t offset,
+//                              uint64_t value, unsigned size)
+// {
+// }
 
-static const MemoryRegionOps stm32_gpio_ops = {
-    .read = stm32_gpio_read,
-    .write = stm32_gpio_write,
-    .valid.min_access_size = 4,
-    .valid.max_access_size = 4,
-    .endianness = DEVICE_NATIVE_ENDIAN};
+// static const MemoryRegionOps stm32_gpio_ops = {
+//     .read = stm32_gpio_read,
+//     .write = stm32_gpio_write,
+//     .valid.min_access_size = 4,
+//     .valid.max_access_size = 4,
+//     .endianness = DEVICE_NATIVE_ENDIAN};
 
-static void feabhas_gpio_init(Object *obj)
-{
-    DeviceState *dev = DEVICE(obj);
-    feabhas_gpio_state_t *s = OBJECT_CHECK(feabhas_gpio_state_t, dev, "feabhas-gpio");
-    SysBusDevice *sbd = SYS_BUS_DEVICE(obj);
+// static void feabhas_gpio_init(Object *obj)
+// {
+//     DeviceState *dev = DEVICE(obj);
+//     feabhas_gpio_state_t *s = OBJECT_CHECK(feabhas_gpio_state_t, dev, "feabhas-gpio");
+//     SysBusDevice *sbd = SYS_BUS_DEVICE(obj);
 
-    for (unsigned pin = 0; pin < STM32_GPIO_PIN_COUNT; ++pin)
-    {
-        sysbus_init_irq(sbd, &s->irq[pin]);
-    }
-    qdev_init_gpio_out(dev, s->irq, STM32_GPIO_PIN_COUNT);
+//     for (unsigned pin = 0; pin < STM32_GPIO_PIN_COUNT; ++pin)
+//     {
+//         sysbus_init_irq(sbd, &s->irq[pin]);
+//     }
+//     qdev_init_gpio_out(dev, s->irq, STM32_GPIO_PIN_COUNT);
 
-    memory_region_init_io(&s->iomem, obj, &stm32_gpio_ops, s,
-                          "gpio", 0x03ff);
-    sysbus_init_mmio(sbd, &s->iomem);
-}
+//     memory_region_init_io(&s->iomem, obj, &stm32_gpio_ops, s,
+//                           "gpio", 0x03ff);
+//     sysbus_init_mmio(sbd, &s->iomem);
+// }
 
-static void feabhas_gpio_class_init(ObjectClass *klass, void *data)
-{
-    DeviceClass *dc = DEVICE_CLASS(klass);
-    dc->vmsd = &vmstate_feabhas_gpio;
-    dc->desc = "Feabhas GPIO";
-}
+// static void feabhas_gpio_class_init(ObjectClass *klass, void *data)
+// {
+//     DeviceClass *dc = DEVICE_CLASS(klass);
+//     dc->vmsd = &vmstate_feabhas_gpio;
+//     dc->desc = "Feabhas GPIO";
+// }
 
-static const TypeInfo feabhas_gpio_info = {
-    .name = "feabhas-gpio",
-    .parent = TYPE_SYS_BUS_DEVICE,
-    .instance_size = sizeof(feabhas_gpio_state_t),
-    .instance_init = feabhas_gpio_init,
-    .class_init = feabhas_gpio_class_init,
-};
+// static const TypeInfo feabhas_gpio_info = {
+//     .name = "feabhas-gpio",
+//     .parent = TYPE_SYS_BUS_DEVICE,
+//     .instance_size = sizeof(feabhas_gpio_state_t),
+//     .instance_init = feabhas_gpio_init,
+//     .class_init = feabhas_gpio_class_init,
+// };
 
-static void feabhas_gpio_register_types(void)
-{
-    type_register_static(&feabhas_gpio_info);
-}
+// static void feabhas_gpio_register_types(void)
+// {
+//     type_register_static(&feabhas_gpio_info);
+// }
 
-type_init(feabhas_gpio_register_types)
+// type_init(feabhas_gpio_register_types)
